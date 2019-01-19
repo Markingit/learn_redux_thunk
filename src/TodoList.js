d8af132497dbb682a5cc3637c65fb0d67bd5241c
matengfei <matengfei@imzhiliao.com>
2019-01-19 10:41:45 +0800

    add .

---
+++ src/TodoList.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react'
+
+import 'antd/dist/antd.css'
+import { Input, Button, List } from 'antd';
+import store from './store/index'
+import { CHANGE_INPIUT_VALUE, ADD_TODO_LIST, DELETE_TODO_LIST } from './store/actionTypes'
+class TodoList extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = store.getState()
+        this.handleInputChange = this.handleInputChange.bind(this)
+        this.handleChange = this.handleChange.bind(this)
+        this.handleBtnclick = this.handleBtnclick.bind(this) 
+        store.subscribe(this.handleChange)// 订阅
+    }
+
+    render() {
+        return (
+            <div style={{marginTop: '10px',marginLeft: '10px'}}>
+               <Input 
+                value={this.state.inputValue} 
+                placeholder='info' 
+                style={{width: '300px',marginRight: '10px'}}
+                onChange={this.handleInputChange}
+                />
+               <Button type="primary" onClick={this.handleBtnclick}>提交</Button>
+               <List
+                 style={{marginTop: '10px',width: '300px'}}
+                 bordered
+                 dataSource={this.state.list}
+                 renderItem={(item, index) => (<List.Item onClick={this.handleItemDelete.bind(this, index)}>{item}</List.Item>)}
+               />
+            </div> 
+        )
+    }
+
+    handleInputChange(e) {
+        const action = {
+            type: CHANGE_INPIUT_VALUE,
+            value: e.target.value
+        }
+        store.dispatch(action)
+    }
+
+    handleChange() {
+        this.setState(store.getState())
+    }
+
+    handleBtnclick() {
+        const action = {
+            type: ADD_TODO_LIST
+        };
+        store.dispatch(action)
+    }
+    handleItemDelete(index) {
+        const action = {
+            type: DELETE_TODO_LIST,
+            index
+        }
+        store.dispatch(action)
+    }
+}
+
+export default TodoList
---
+++ src/store/actionTypes.js
@@ -0,0 +1,3 @@
+export const CHANGE_INPIUT_VALUE = 'change_input_value'
+export const ADD_TODO_LIST = 'add_todo_item'
+export const DELETE_TODO_LIST = 'delete_todo_item'
